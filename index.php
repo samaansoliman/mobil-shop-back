bf13b2a97027a4966f40983faa7c70209d09014f
Your Name <you@example.com>
2021-07-14 21:35:29 +0200

    mobil-shop-back

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// include header.php file
+include ('header.php');
+?>
+
+
+<?php
+// include banner-area
+include ('Template/_banner-area.php');
+// include banner-area
+// include top-sale
+include ('Template/_top-sale.php');
+// include top-sale
+// include special-price
+include ('Template/_special-price.php');
+// include special-price
+// include banner-adds.php
+include ('Template/_banner-adds.php');
+// include banner-adds.php
+// include new-phones.php
+include ('Template/_new-phones.php');
+// include new-phones.php
+// include blogs.php
+include ('Template/_blogs.php');
+// include blogs.php
+?>
+
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
